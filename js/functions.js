0fe5c110aa0b05692492d6484b6dd2b2483896d6
jennioksman <c3okje00@students.oamk.fi>
2024-02-13 13:52:57 +0200

    Tehtävälista tarkistusta varten.

---
+++ js/functions.js
@@ -0,0 +1,43 @@
+const list = document.querySelector('table')
+const input = document.querySelector('input')
+const span = document.querySelector('span')
+const todos = []
+
+
+const deleteRow = (todoName) => {
+    const index =todos.indexOf(todoName)
+    todos.slice(index,1)
+    list.deleteRow(index)
+}
+
+const addTableRow = (todoName) => {
+    const tr = list.insertRow()
+        const td1 = tr.insertCell(0)
+        td1.innerHTML = todoName
+        const td2 = tr.insertCell(1)
+        td2.innerHTML = '<a href="#" onClick="deleteRow(\''+ todoName + '\')">x<a/>'
+}
+
+span.addEventListener('click', () => {
+    todos.sort()
+
+    for (let i= todos.length-1;i>=0;i--) {
+        list.deleteRow(i)
+    
+    todos.forEach((todo) => {
+        addTableRow(todo)
+    })
+    }
+})
+input.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        const newTodo = input.value
+        todos.push(newTodo)
+        const tr = list.insertRow()
+        const td1 = tr.insertCell(0)
+        td1.innerHTML = newTodo
+        const td2 = tr.insertCell(1)
+        td2.innerHTML = '<a href="#" onClick="deleteRow(\''+ newTodo + '\')">x<a/>'
+        input.value = ''
+    }
+})
